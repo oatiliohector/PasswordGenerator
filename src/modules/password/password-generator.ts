6037761ac82da5e2891f609a26257ed6266bd331
Atilio <atilio.jesus@grupoboticario.com.br>
2024-08-04 16:58:58 -0300

    refactor: ♻︎ ajusta lint do password-generator.ts

--- src/modules/password/password-generator.ts
+++ src/modules/password/password-generator.ts
@@ -2,24 +2,24 @@ import { PokemonApi } from "../pokemon/pokemon-api";
 
 export class PasswordGenerator {
 
-    private generateRandomNumber(){
-        const number = Math.floor(Math.random() * 500);
-        return number;
-    }
+  private generateRandomNumber(){
+      const number = Math.floor(Math.random() * 500);
+      return number;
+  }
 
-    public async generatePassword(): Promise<string> {
+  public async generatePassword(): Promise<string> {
 
-      const chamadaPokemon = new PokemonApi('https://pokeapi.co/api/v2/pokemon');
-      try {
-        const randomNumber = this.generateRandomNumber();
+    const chamadaPokemon = new PokemonApi('https://pokeapi.co/api/v2/pokemon');
+    try {
+      const randomNumber = this.generateRandomNumber();
 
-        const pokemons = await chamadaPokemon.getPokemonData(randomNumber, randomNumber);
-        const pokemonName = pokemons[0].name;
+      const pokemons = await chamadaPokemon.getPokemonData(randomNumber, randomNumber);
+      const pokemonName = pokemons[0].name;
 
-        return pokemonName;
-      } catch (error) {
-        console.error('Erro ao gerar senha:', error);
-        return '';
-      }
+      return pokemonName;
+    } catch (error) {
+      console.error('Erro ao gerar senha:', error);
+      return '';
     }
   }
+}
